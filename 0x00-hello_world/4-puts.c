0d4a5e06918f7d859f9edfe1df3224f1a5e9e6a0
salihat ibrahim <salihatib60@gmail.com>
2023-02-17 09:46:09 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
-#include<stdio.h>
-/
+#include <stdio.h>
+
+/**
  * main - Entry point
- *
- * Return: Always 0 (success)
+ * Description: prints "\"Programming is like building a multilingual puzzle"
+ * Return: 0
  */
+
 int main(void)
-{       
-        puts("\"programming is like building a multilingual
-puzzle");
+{
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
