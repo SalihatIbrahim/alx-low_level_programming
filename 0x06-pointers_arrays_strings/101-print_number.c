a4c074a0a603e64a5d97c9aed348231ddcbb692b
salihat ibrahim <salihatib60@gmail.com>
2023-03-02 08:33:16 -0800

    101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,15 +1,36 @@
-#include "holberton.h"
-/**
- * set_string - sets the value of a pointer to a char
- *
- * @s: The pointer to change
- * @to: The value to change the pointer to
- *
- * Return: nothing
- */
+#include "main.h"
 
-void set_string(char **s, char *to)
+/**
+ * print_number - printing number with puts
+ *
+ * @n: input int
+ */
+void print_number(int n)
 {
-	*s = to;
+	/*Declaring variables*/
+	int count = 0, pow = 1;
+	unsigned int num = n;
+
+
+	if (n < 0) /*Evaluate this condition*/
+	{
+		_putchar('-');
+		num = -n;
+	}
+	while (n != 0)
+	{
+		n /= 10;
+		count++;
+	}
+	while (count > 1)
+	{
+		pow *= 10;
+		count--;
+	}
+	while (pow >= 1)
+	{
+		_putchar(num / pow % 10 + '0');
+		pow /= 10;
+	}
 }
 
