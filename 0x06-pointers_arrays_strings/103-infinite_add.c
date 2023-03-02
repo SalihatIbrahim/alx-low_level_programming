50665e7e47f09bd1337f45054c57413f4137398a
salihat ibrahim <salihatib60@gmail.com>
2023-03-02 08:51:25 -0800

    103-infinite_add.c

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,11 +1,35 @@
-#include "holberton.h"
-/**
- * print_buffer - add 2 strings.
- * @b: string1.
- * @size: string2.
- * Return: String with all letters in ROT13 base.
- */
-void print_buffer(char *b, int size)
-{
+#include "main.h"
 
+/**
+ * print_number - printing number with puts
+ *
+ * @n: input int
+ */
+void print_number(int n)
+{
+	/*Declaring variables*/
+	int count = 0, pow = 1;
+	unsigned int num = n;
+
+
+	if (n < 0) /*Evaluate this condition*/
+	{
+		_putchar('-');
+		num = -n;
+	}
+	while (n != 0)
+	{
+		n /= 10;
+		count++;
+	}
+	while (count > 1)
+	{
+		pow *= 10;
+		count--;
+	}
+	while (pow >= 1)
+	{
+		_putchar(num / pow % 10 + '0');
+		pow /= 10;
+	}
 }
