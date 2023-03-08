7739f73a7524d9eb36b6b382e9a8269d05b55973
salihat ibrahim <salihatib60@gmail.com>
2023-03-08 10:40:47 -0800

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,19 +1,61 @@
 #include "holberton.h"
+
 /**
- * wildcmp -  compare 2 string and search if s2 is a valid wildcard string
- * @s1: string.
- * @s2: string with wildcard.
- * Return: 1 if the strings can be considered identical, otherwise return 0.
+ * strlen_rec - String length
+ *
+ * @str: char
+ *
+ * Return: the integer length
  */
-int wildcmp(char *s1, char *s2)
+
+int strlen_rec(char *str)
 {
-	if (*s1 == '\0' && *s2 == '*' && *(s2 + 1) != '\0')
-		return (0);
-	if (*s1 == '\0' && *s2 == '\0')
-		return (1);
-	if (*s1 == *s2)
-		return (wildcmp(s1 + 1, s2 + 1));
-	if (*s2 == '*')
-		return (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2));
+	if (*str)
+	{
+		str++;
+
+		return (1 + strlen_rec(str));
+	}
 	return (0);
+}
+
+/**
+ * helper_pal - Returns Palindrone
+ *
+ * @str: char
+ *
+ * @length: int
+ *
+ * @count: int
+ *
+ * Return: the integer length
+ */
+
+int helper_pal(char *str, int length, int count)
+{
+	if (count >= length)
+	{
+		return (1);
+	}
+	if (str[length] == str[count])
+	{
+		return (helper_pal(str, length - 1, count + 1));
+	}
+	return (0);
+}
+
+/**
+ * is_palindrome - returns 1 if a string is a palindrome and 0 if not
+ *
+ * @s: char
+ *
+ * Return: the integer length
+ */
+
+int is_palindrome(char *s)
+{
+	int length = strlen_rec(s);
+	int count = 0;
+
+	return (helper_pal(s, length - 1, count));
 }
